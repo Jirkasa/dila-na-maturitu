052c6704c710e207ad49262953c3bcf23293c806
Jiří <satorajirka@seznam.cz>
2022-07-03 15:00:32 +0200

    install postgress driver

--- server/src/services/database.js
+++ server/src/services/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 // create Sequelize instance
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-    dialect: "mysql",
+    dialect: process.env.DATABASE_DIALECT,
     host: process.env.DATABASE_HOST
 });
 
